c48172354412730c52f4f498b443a3485792579b
Rajendra Kulkarni <rajerkulkarni01@gmail.com>
2024-11-22 23:29:01 +0530

    Good enough to deploy

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Countrycard.css";
 import { Country } from "../App";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
